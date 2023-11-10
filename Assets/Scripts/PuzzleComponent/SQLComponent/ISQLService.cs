bcb9724c9825070f27f2979183feb96472f758a3
salaza <gun159@outlook.com>
2023-11-10 14:49:42 +0700

    Make sql service

---
+++ Assets/Scripts/PuzzleComponent/SQLComponent/ISQLService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.PuzzleComponent.SQLComponent
+{
+    public interface ISQLService
+    {
+        string[][] GetTableResult(string dbPath, string sql, PuzzleType puzzleType);
+    }
+}
