b6570d7d09f068dc9f176b6b4c254df5a471f809
salaza <gun159@outlook.com>
2023-11-11 14:58:12 +0700

    Create mission generator

---
+++ Assets/Scripts/MissionGenComponent/MissionGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.MissionGenComponent
+{
+    public class MissionGenerator : MonoBehaviour
+    {
+        [SerializeField] private TextAsset _configFile;
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
