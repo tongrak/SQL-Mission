f8b4a46c428ae9d9072173eb481c9a640e1db5c0
Oliver Tongrak <oliver.tongrak@gmail.com>
2023-12-16 22:48:10 +0700

    Add UI button scripts
    Note: that tab button controller is considered tobe placeholder

---
+++ Assets/Scripts/UI/GeneralActionButtonController.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.UI;
+
+namespace Gameplay.UI
+{
+    public enum ActionButtonType { EXECUTION, PROCEED, STEP_BACK }
+
+    public interface IActionButtonController
+    {
+        bool Activivity { get; set; }
+        ActionButtonType ActionButtonType { get; set; }
+
+    }
+
+    public class GeneralActionButtonController : GameplayController, IActionButtonController
+    {
+        [Header("Action Button Resources")]
+        [SerializeField] private Sprite _executionSprite;
+        [SerializeField] private Sprite _disableExecutionSprite;
+        [SerializeField] private Sprite _proceedSprite;
+        [SerializeField] private Sprite _stepBackSprite;
+        [Header("Button Image")]
+        [SerializeField] private UnityEngine.UI.Image _buttonImage;
+
+
+        private Sprite getFromActionButtonType(ActionButtonType givenType)
+        {
+            switch (givenType)
+            {
+                case ActionButtonType.EXECUTION: return _executionSprite;
+                case ActionButtonType.PROCEED: return _proceedSprite;
+                case ActionButtonType.STEP_BACK: return _stepBackSprite;
+            }
+            throw new System.Exception("Given type is invalid");
+        }
+
+        private bool _isActive = false;
+        public bool Activivity
+        {
+            get => _isActive;
+            set
+            {
+                _isActive = value;
+                _buttonImage.sprite = _isActive ? getFromActionButtonType(_currentType) : _disableExecutionSprite;
+            }
+        }
+        private ActionButtonType _currentType;
+        public ActionButtonType ActionButtonType
+        {
+            get => _currentType;
+            set
+            {
+                _currentType = value;
+                _buttonImage.sprite = _isActive ? getFromActionButtonType(_currentType) : _disableExecutionSprite;
+            }
+        }
+
+        private void Start()
+        {
+            Activivity = false;
+            ActionButtonType = ActionButtonType.EXECUTION;
+        }
+    }
+}
+
+
---
+++ Assets/Scripts/UI/TabsButtonController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Gameplay.UI
+{
+    public interface IConsoleTabsController
+    {
+        TabType CurrentTab { get; set; }
+    }
+
+    public class TabsButtonController : GameplayController, IConsoleTabsController
+    {
+        //TODO: Visual feedback improvement
+
+        private TabType _selectedTab = TabType.CONSTRUCT;
+        public TabType CurrentTab { get => _selectedTab; set => _selectedTab = value; }
+    }
+}
