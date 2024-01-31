252e99083f0de3cef9664a48d83d5ec114e686bf
salaza <gun159@outlook.com>
2024-01-31 21:00:48 +0700

    Fix init file watcher method

--- Assets/Scripts/BackendComponent/MissionGenerator.cs
+++ Assets/Scripts/BackendComponent/MissionGenerator.cs
@@ -50,7 +50,7 @@ namespace Assets.Scripts.DataPersistence
                 InitiateMissionStatusFileWatcher();
             }
 
-            if (_selectedChapterData.IsPassed)
+            if (!_selectedChapterData.IsPassed)
             {
                 InitiateChapterStatusFileWatcher();
             }
