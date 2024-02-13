02261b4a676506512a1ae080b47f2037b5497cb0
Oliver Tongrak <oliver.tongrak@gmail.com>
2024-02-14 00:16:18 +0700

    Add give up button to Placement test

---
+++ Assets/Scripts/UI/PopUp/PlacementPopUpManager.cs
@@ -0,0 +1,50 @@
+﻿using Gameplay;
+using Gameplay.UI.PopUp;
+using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.UI.PopUp
+{
+    public interface IPlacementPopUpManager : IPopUpsManager
+    {
+        void DisplayGiveUpMenu();
+        void HideGiveUpMenu();
+    }
+    public class PlacementPopUpManager : GameplayController, IPlacementPopUpManager
+    {
+        [Header("Gameobjects")]
+        [SerializeField] private GameObject _giveUpMenuGO;
+        [SerializeField] protected GameObject _softLoadingGO;
+        [SerializeField] private GameObject _popUpShadowGO;
+
+        public void initPopUp()
+        {
+            _giveUpMenuGO.SetActive(false);
+            _softLoadingGO.SetActive(false);
+
+            _popUpShadowGO.SetActive(true);
+        }
+
+        public void DisplayGiveUpMenu()
+        {
+            this.gameObject.SetActive(true);
+            this.initPopUp();
+            _giveUpMenuGO.SetActive(true);
+            _popUpShadowGO.GetComponent<UnityEngine.UI.Button>().interactable = true;
+        }
+
+        public void DisplaySoftLoading()
+        {
+            this.initPopUp();
+            this._softLoadingGO.SetActive(true);
+        }
+
+        public void HideGiveUpMenu()
+        {
+            _giveUpMenuGO.SetActive(false);
+            _popUpShadowGO.SetActive(false);
+        }
+
+        public void DisplayCompletionMenu(bool canContinueMission){}
+    }
+}
--- Assets/Scripts/UI/PopUp/PopUpsManager.cs
+++ Assets/Scripts/UI/PopUp/PopUpsManager.cs
@@ -4,27 +4,30 @@ using UnityEngine;
 
 namespace Gameplay.UI.PopUp
 {
-    public interface IPopUpsManager
+    public interface AbleToSoftLoad
     {
-        void DisplayGameplayMenu();
-        void DisplayCompletionMenu(bool canContinueMission);
         void DisplaySoftLoading();
+    }
+    public interface IPopUpsManager: AbleToSoftLoad
+    {
+        //void DisplayGameplayMenu();
+        void DisplayCompletionMenu(bool canContinueMission);
     }
     public class PopUpsManager : GameplayController, IPopUpsManager
     {
-        [Header("Gameobject")]
+        [Header("Gameobjects")]
         [SerializeField] private GameObject _gameplayMenuGO;
         [SerializeField] private GameObject _completionMenuGO;
         [SerializeField] private GameObject _contiueButtonGO;
-        [SerializeField] private GameObject _softLoadingGO;
-        [SerializeField] private GameObject _popUpShawdowGO;
+        [SerializeField] protected GameObject _softLoadingGO;
+        [SerializeField] protected GameObject _popUpShawdowGO;
 
         [Header("Continue button sprite")]
         [SerializeField] private Sprite _activeContinueButton;
         [SerializeField] private Sprite _inactiveContinueButton;
 
         #region AUX methods
-        private void initAllPopUp()
+        protected virtual void initAllPopUp()
         {
             //Hide all popUps
             _gameplayMenuGO.SetActive(false);
@@ -66,8 +69,6 @@ namespace Gameplay.UI.PopUp
             this.initAllPopUp();
             this._softLoadingGO.SetActive(true);
         }
-
-        
     }
 }
 
