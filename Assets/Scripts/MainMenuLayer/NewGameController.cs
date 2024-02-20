d80188d4f59ce389917eb2039220b2059d5692d8
Oliver Tongrak <oliver.tongrak@gmail.com>
2024-02-20 22:40:30 +0700

    Redesign MainMenu scene

--- Assets/Scripts/MainMenuLayer/NewGameController.cs
+++ Assets/Scripts/MainMenuLayer/NewGameController.cs
@@ -3,10 +3,12 @@ using Assets.Scripts.MainMenuLayer;
 
 public class NewGameController : MonoBehaviour
 {
+    [SerializeField] private GameObject _confirmationPopUp;
     [SerializeField] private MainMenuButtonManager _mainMenuButtonManager;
 
-    public void NewGameButtonClicked()
-    {
-        _mainMenuButtonManager.NewGameButtonClicked();
-    }
+    public void NewGameButtonClicked() => _confirmationPopUp.SetActive(true);
+    public void ConfirmNewGame() => _mainMenuButtonManager.NewGameButtonClicked();
+    public void CancelNewGame() => _confirmationPopUp.SetActive(false);
+
+    private void Start() => _confirmationPopUp.SetActive(false);
 }
